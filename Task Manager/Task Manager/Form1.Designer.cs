ba31642c65315d4d04d053bb0895ed4e848f7a9d
Nguyễn Xuân Phi <ngonphikp@gmail.com>
2019-05-03 16:09:02 +0700

    Demo Task Manager

---
+++ Task Manager/Task Manager/Form1.Designer.cs
@@ -0,0 +1,250 @@
+﻿namespace Task_Manager
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.lbxProcess = new System.Windows.Forms.ListBox();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.killProcessToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.lbProcess = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.btnCreateProcess = new System.Windows.Forms.Button();
+            this.tbUD = new System.Windows.Forms.TextBox();
+            this.cbbLoai = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.tbTT = new System.Windows.Forms.TextBox();
+            this.tbLink = new System.Windows.Forms.TextBox();
+            this.btnKillProcess = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.lbTimeKill = new System.Windows.Forms.Label();
+            this.contextMenuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // lbxProcess
+            // 
+            this.lbxProcess.ContextMenuStrip = this.contextMenuStrip1;
+            this.lbxProcess.FormattingEnabled = true;
+            this.lbxProcess.ItemHeight = 16;
+            this.lbxProcess.Location = new System.Drawing.Point(12, 46);
+            this.lbxProcess.Name = "lbxProcess";
+            this.lbxProcess.Size = new System.Drawing.Size(206, 404);
+            this.lbxProcess.TabIndex = 0;
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.killProcessToolStripMenuItem});
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(153, 28);
+            // 
+            // killProcessToolStripMenuItem
+            // 
+            this.killProcessToolStripMenuItem.Name = "killProcessToolStripMenuItem";
+            this.killProcessToolStripMenuItem.Size = new System.Drawing.Size(152, 24);
+            this.killProcessToolStripMenuItem.Text = "Kill Process";
+            this.killProcessToolStripMenuItem.Click += new System.EventHandler(this.killProcessToolStripMenuItem_Click);
+            // 
+            // lbProcess
+            // 
+            this.lbProcess.AutoSize = true;
+            this.lbProcess.Location = new System.Drawing.Point(159, 15);
+            this.lbProcess.Name = "lbProcess";
+            this.lbProcess.Size = new System.Drawing.Size(16, 17);
+            this.lbProcess.TabIndex = 3;
+            this.lbProcess.Text = "0";
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // btnCreateProcess
+            // 
+            this.btnCreateProcess.Location = new System.Drawing.Point(312, 252);
+            this.btnCreateProcess.Name = "btnCreateProcess";
+            this.btnCreateProcess.Size = new System.Drawing.Size(132, 25);
+            this.btnCreateProcess.TabIndex = 4;
+            this.btnCreateProcess.Text = "Create Process";
+            this.btnCreateProcess.UseVisualStyleBackColor = true;
+            this.btnCreateProcess.Click += new System.EventHandler(this.btnCreateProcess_Click);
+            // 
+            // tbUD
+            // 
+            this.tbUD.Location = new System.Drawing.Point(312, 46);
+            this.tbUD.Name = "tbUD";
+            this.tbUD.Size = new System.Drawing.Size(132, 22);
+            this.tbUD.TabIndex = 5;
+            // 
+            // cbbLoai
+            // 
+            this.cbbLoai.FormattingEnabled = true;
+            this.cbbLoai.Items.AddRange(new object[] {
+            "UD / TT",
+            "Web"});
+            this.cbbLoai.Location = new System.Drawing.Point(312, 199);
+            this.cbbLoai.Name = "cbbLoai";
+            this.cbbLoai.Size = new System.Drawing.Size(132, 24);
+            this.cbbLoai.TabIndex = 6;
+            this.cbbLoai.Text = "Lựa chọn loại";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 15);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(141, 17);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "Danh Sách Process: ";
+            // 
+            // tbTT
+            // 
+            this.tbTT.Location = new System.Drawing.Point(312, 96);
+            this.tbTT.Name = "tbTT";
+            this.tbTT.Size = new System.Drawing.Size(132, 22);
+            this.tbTT.TabIndex = 10;
+            // 
+            // tbLink
+            // 
+            this.tbLink.Location = new System.Drawing.Point(312, 153);
+            this.tbLink.Name = "tbLink";
+            this.tbLink.Size = new System.Drawing.Size(132, 22);
+            this.tbLink.TabIndex = 12;
+            // 
+            // btnKillProcess
+            // 
+            this.btnKillProcess.Location = new System.Drawing.Point(312, 301);
+            this.btnKillProcess.Name = "btnKillProcess";
+            this.btnKillProcess.Size = new System.Drawing.Size(132, 25);
+            this.btnKillProcess.TabIndex = 13;
+            this.btnKillProcess.Text = "Kill Process";
+            this.btnKillProcess.UseVisualStyleBackColor = true;
+            this.btnKillProcess.Click += new System.EventHandler(this.btnKillProcess_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(224, 46);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(74, 17);
+            this.label1.TabIndex = 14;
+            this.label1.Text = "Ứng dụng:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(224, 99);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(56, 17);
+            this.label3.TabIndex = 15;
+            this.label3.Text = "Tệp tin:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(232, 158);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(38, 17);
+            this.label4.TabIndex = 16;
+            this.label4.Text = "Link:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(224, 15);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(150, 17);
+            this.label5.TabIndex = 17;
+            this.label5.Text = "Nhập đường dẫn / tên ";
+            // 
+            // lbTimeKill
+            // 
+            this.lbTimeKill.AutoSize = true;
+            this.lbTimeKill.Font = new System.Drawing.Font("Microsoft Sans Serif", 25.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbTimeKill.ForeColor = System.Drawing.Color.Red;
+            this.lbTimeKill.Location = new System.Drawing.Point(233, 274);
+            this.lbTimeKill.Name = "lbTimeKill";
+            this.lbTimeKill.Size = new System.Drawing.Size(47, 52);
+            this.lbTimeKill.TabIndex = 18;
+            this.lbTimeKill.Text = "0";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(482, 450);
+            this.Controls.Add(this.lbTimeKill);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnKillProcess);
+            this.Controls.Add(this.tbLink);
+            this.Controls.Add(this.tbTT);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.cbbLoai);
+            this.Controls.Add(this.tbUD);
+            this.Controls.Add(this.btnCreateProcess);
+            this.Controls.Add(this.lbProcess);
+            this.Controls.Add(this.lbxProcess);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.contextMenuStrip1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox lbxProcess;
+        private System.Windows.Forms.Label lbProcess;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem killProcessToolStripMenuItem;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Button btnCreateProcess;
+        private System.Windows.Forms.TextBox tbUD;
+        private System.Windows.Forms.ComboBox cbbLoai;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox tbTT;
+        private System.Windows.Forms.TextBox tbLink;
+        private System.Windows.Forms.Button btnKillProcess;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label lbTimeKill;
+    }
+}
+
---
+++ Task Manager/Task Manager/Form1.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+using System.Diagnostics;
+
+namespace Task_Manager
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        Process[] procs;
+        private void GetProcesses()
+        {
+            procs = Process.GetProcesses(); //Trả về tất cả các tiến trình đang chạy trên máy tính
+
+            if (Convert.ToInt32(lbProcess.Text) != procs.Length)
+            {
+                lbxProcess.Items.Clear();
+                for (int i = 0; i < procs.Length; i++)
+                {
+                    lbxProcess.Items.Add("> " + procs[i].ProcessName); // Tạo danh sách tương tự task manager
+                }                
+            }
+            lbProcess.Text = procs.Length.ToString();
+        }
+
+        private void KillProcess(int index)
+        {
+            if(index < 0 || index >= procs.Length) //TH khi không chọn item
+            {
+                MessageBox.Show("Out Size");
+                return;
+            }
+            procs[index].WaitForExit(3000); //Đợi 3s rồi tắt
+            procs[index].Kill();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            GetProcesses();
+        }        
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            GetProcesses();
+        }
+
+        private void killProcessToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            KillProcess(lbxProcess.SelectedIndex);
+        }
+
+        // Tương tự window run(window + R)
+        private void btnCreateProcess_Click(object sender, EventArgs e)
+        {
+            string ud = tbUD.Text;
+            string tt = tbTT.Text;
+            string link = tbLink.Text;
+            int loai = cbbLoai.SelectedIndex;
+
+            try
+            {
+                switch (loai)
+                {
+                    //Nếu ứng dụng / tệp tin
+                    case 0:
+                        if (tt == "")
+                            Process.Start(ud);
+                        // Mở file = ứng dụng mặc định
+                        else if(ud == "")
+                            Process.Start(tt);
+                        else
+                            Process.Start(ud, tt);
+                        break;
+                    //Nếu web
+                    case 1:
+                        // Trình duyệt mặc định
+                        if (ud == "")
+                            Process.Start(@link);
+                        else
+                            Process.Start(ud, @link);
+                        break;
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error");
+            }
+            
+        }
+
+        private void btnKillProcess_Click(object sender, EventArgs e)
+        {
+            KillProcess(lbxProcess.SelectedIndex);
+        }
+    }
+}
